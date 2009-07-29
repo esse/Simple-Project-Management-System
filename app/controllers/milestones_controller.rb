0ccc4bc8575d34a8c9bf80f3d89adc1dca3fba76
Piotr Szmielew <esse@weblify.pl>
2009-07-29 19:52:48 +0200

    edit/removing milestones feature

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,6 +1,7 @@
 class MilestonesController < ApplicationController
   before_filter :get_project
-
+  deny :user => :is_guest?
+  allow :user => :is_authorized?, :object => :project
   def index
     @milestones = @project.milestones
   end
@@ -24,15 +25,27 @@ class MilestonesController < ApplicationController
   end
 
   def edit
-
+    @milestone = @project.milestones.find(params[:id])
   end
 
   def update
-
+    @milestone = @project.milestones.find(params[:id])
+    @milestone.update_attributes!(params[:milestone].merge(:project_id => @project.id, :user_id => @user.id))
+    flash[:notice] = 'Uaktualniono punkt przełomowy'
+    redirect_to project_milestones_path(@project.id)
+  rescue
+    flash[:error] = 'Wystąpił błąd'
+    render :action => :edit
   end
 
   def destroy
-    
+    @milestone = @project.milestones.find(params[:id])
+    @milestone.destroy!
+    flash[:notice] = 'Usunięto punkt przełomowy'
+    redirect_to project_milestones_path(@milestone.project)
+  rescue
+    flash[:error] = 'Wystąpił błąd'
+    redirect_to project_milestone_path(@milestone.project.id, @milestone.id)
   end
 
 end
